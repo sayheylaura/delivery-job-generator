0c805f1f627f8fcdaa94ec51106b01a3d49b40a4
Laura Ferrando <lauraferrandof@gmail.com>
2021-04-22 14:23:32 +0200

    chore: add normalize.sass

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,8 +2,19 @@ module.exports = {
 	defaultSeverity: 'warning',
 	extends: 'stylelint-config-standard',
 	plugins: ['stylelint-order', 'stylelint-scss'],
+	ignoreFiles: ['./node_modules', './assets/styles/base/_normalize.sass'],
 	rules: {
-		'order/properties-alphabetical-order': true,
-		'declaration-block-trailing-semicolon': 'never'
+		'block-closing-brace-empty-line-before': null,
+		'block-closing-brace-newline-after': null,
+		'block-closing-brace-newline-before': null,
+		'block-closing-brace-space-after': null,
+		'block-closing-brace-space-before': null,
+		'block-opening-brace-newline-after': null,
+		'block-opening-brace-newline-before': null,
+		'block-opening-brace-space-after': null,
+		'block-opening-brace-space-before': null,
+		'declaration-block-trailing-semicolon': 'never',
+		'number-leading-zero': 'never',
+		'order/properties-alphabetical-order': true
 	}
 };
