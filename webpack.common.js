38a240885fbc836e6e85654d764e3f4347279408
Laura Ferrando <lauraferrandof@gmail.com>
2021-04-27 18:17:09 +0200

    feat: add favicon

--- webpack.common.js
+++ webpack.common.js
@@ -57,7 +57,8 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, './public/index.html')
+			template: path.resolve(__dirname, './public/index.html'),
+			favicon: path.resolve(__dirname, './public/favicon-32x32.png')
 		}),
 		new SpriteLoaderPlugin(),
 		new webpack.DefinePlugin({
