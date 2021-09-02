0b3b2a1a6dd96d6eb48701422ea73f3175e67fe7
Laura Ferrando <lauraferrandof@gmail.com>
2021-09-02 09:05:14 +0200

    chore: update webpack plugins and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -24,13 +24,11 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true,
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-						plugins: ['@babel/plugin-transform-runtime']
-					}
+				loader: 'babel-loader',
+				options: {
+					cacheDirectory: true,
+					presets: ['@babel/preset-env', '@babel/preset-react'],
+					plugins: ['@babel/plugin-transform-runtime']
 				}
 			},
 			{
